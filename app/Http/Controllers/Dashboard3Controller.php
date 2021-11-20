83da922b6b86d59d60892e4a989894579f5f6536
HanifRoyyan <60511929+hnfroy@users.noreply.github.com>
2021-11-20 16:10:17 +0700

    Update Dashboard3Controller.php

--- app/Http/Controllers/Dashboard3Controller.php
+++ app/Http/Controllers/Dashboard3Controller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\presensi;
+use Illuminate\Support\Facades\Auth;
 
 class Dashboard3Controller extends Controller
 {
@@ -15,6 +16,7 @@ class Dashboard3Controller extends Controller
     public function index()
     {
         $presensi = presensi::latest()->paginate(5);
+        $presensi = presensi::where('user_id', Auth::user()->id)->get();
     
         return view('dashboard3.index',compact('presensi'))
             ->with('i', (request()->input('page', 1) - 1) * 5);
